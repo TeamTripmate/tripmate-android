9a791261aad9eb34fcc1d62c649cd6b87989650d
Jeong Sang Hoon <pathfinder8851@gmail.com>
2024-07-23 17:06:34 +0900

    [style] Color,Font,Theme,Type 추가

---
+++ core/designsystem/src/main/kotlin/com/tripmate/android/core/designsystem/theme/Font.kt
@@ -0,0 +1,22 @@
+package com.tripmate.android.core.designsystem.theme
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.tripmate.android.core.designsystem.R
+
+val pretendardFamily = FontFamily(
+    Font(R.font.pretendard_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.pretendard_semi_bold, FontWeight.SemiBold, FontStyle.Normal),
+    Font(R.font.pretendard_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.pretendard_regular, FontWeight.Normal, FontStyle.Normal),
+)
+
+val Title0 = TextStyle(
+    fontFamily = pretendardFamily,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 24.sp,
+)
---
+++ core/designsystem/src/main/kotlin/com/tripmate/android/core/designsystem/theme/Theme.kt
@@ -0,0 +1,47 @@
+package com.tripmate.android.core.designsystem.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Primary01Dark,
+    secondary = Primary02Dark,
+    tertiary = Primary03Dark,
+    background = Background01Dark,
+    surface = Background02Dark,
+    onPrimary = Gray009Dark,
+    onSecondary = Gray009Dark,
+    onTertiary = Gray009Dark,
+    onBackground = Gray001Dark,
+    onSurface = Gray001Dark,
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = Primary01,
+    secondary = Primary02,
+    tertiary = Primary03,
+    background = Background01,
+    surface = Background02,
+    onPrimary = Gray009,
+    onSecondary = Gray009,
+    onTertiary = Gray009,
+    onBackground = Gray001,
+    onSurface = Gray001,
+)
+
+@Composable
+fun TripmateTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit,
+) {
+    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content,
+    )
+}
