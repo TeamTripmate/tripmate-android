4dca0e43a13f1fad760e9574b8a6c91f78e77c5f
Jeong Sang Hoon <pathfinder8851@gmail.com>
2024-07-24 17:41:29 +0900

    [setting] 레포지토리 모듈 작성

---
+++ core/data/src/main/kotlin/com/tripmate/android/core/data/di/RepositoryModule.kt
@@ -0,0 +1,20 @@
+package com.tripmate.android.core.data.di
+
+import com.tripmate.android.core.data.repository.OnboardingRepository
+import com.tripmate.android.core.data.repository.OnboardingRepositoryImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindOnboardingRepository(onboardingRepositoryImpl: OnboardingRepositoryImpl): OnboardingRepository
+
+
+}
