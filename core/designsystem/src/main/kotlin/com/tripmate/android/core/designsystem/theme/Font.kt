f7af3323268fba099e2ecf6c0a3b8a03175c0d69
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-08-05 22:05:15 +0900

    [feat] font 시스템 구축

--- core/designsystem/src/main/kotlin/com/tripmate/android/core/designsystem/theme/Font.kt
+++ core/designsystem/src/main/kotlin/com/tripmate/android/core/designsystem/theme/Font.kt
@@ -10,12 +10,97 @@ import com.tripmate.android.core.designsystem.R
 
 val pretendardFamily = FontFamily(
     Font(R.font.pretendard_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.pretendard_semibold, FontWeight.SemiBold, FontStyle.Normal),
     Font(R.font.pretendard_regular, FontWeight.Normal, FontStyle.Normal),
     Font(R.font.pretendard_light, FontWeight.Light, FontStyle.Normal),
 )
 
-val Title0 = TextStyle(
+val XLarge26_Bold = TextStyle(
     fontFamily = pretendardFamily,
     fontWeight = FontWeight.Bold,
-    fontSize = 24.sp,
+    fontSize = 26.sp,
+)
+
+val XLarge26_Reg = TextStyle(
+    fontFamily = pretendardFamily,
+    fontWeight = FontWeight.Normal,
+    fontSize = 26.sp,
+)
+
+val XLarge26_Light = TextStyle(
+    fontFamily = pretendardFamily,
+    fontWeight = FontWeight.Light,
+    fontSize = 26.sp,
+)
+
+val Large20_Bold = TextStyle(
+    fontFamily = pretendardFamily,
+    fontWeight = FontWeight.Bold,
+    fontSize = 20.sp,
+)
+
+val Large20_Reg = TextStyle(
+    fontFamily = pretendardFamily,
+    fontWeight = FontWeight.Normal,
+    fontSize = 20.sp,
+)
+
+val Large20_Light = TextStyle(
+    fontFamily = pretendardFamily,
+    fontWeight = FontWeight.Light,
+    fontSize = 20.sp,
+)
+
+val Medium16_SemiBold = TextStyle(
+    fontFamily = pretendardFamily,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 16.sp,
+)
+
+val Medium16_Reg = TextStyle(
+    fontFamily = pretendardFamily,
+    fontWeight = FontWeight.Normal,
+    fontSize = 16.sp,
+)
+
+val Medium16_Light = TextStyle(
+    fontFamily = pretendardFamily,
+    fontWeight = FontWeight.Light,
+    fontSize = 16.sp,
+)
+
+val Small14_SemiBold = TextStyle(
+    fontFamily = pretendardFamily,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 14.sp,
+)
+
+val Small14_Reg = TextStyle(
+    fontFamily = pretendardFamily,
+    fontWeight = FontWeight.Normal,
+    fontSize = 14.sp,
+)
+
+val Small14_Light = TextStyle(
+    fontFamily = pretendardFamily,
+    fontWeight = FontWeight.Light,
+    fontSize = 14.sp,
+)
+
+val XSmall12_SemiBold = TextStyle(
+    fontFamily = pretendardFamily,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 12.sp,
+)
+
+val XSmall12_Reg = TextStyle(
+    fontFamily = pretendardFamily,
+    fontWeight = FontWeight.Normal,
+    fontSize = 12.sp,
+)
+
+val XSmall12_Light = TextStyle(
+    fontFamily = pretendardFamily,
+    fontWeight = FontWeight.Light,
+    fontSize = 12.sp,
 )
