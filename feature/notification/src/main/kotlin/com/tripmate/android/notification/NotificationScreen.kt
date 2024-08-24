4fa4da2281ea734e8ab9fd3d74d9a989ca734b33
easyhooon <mraz3068@gmail.com>
2024-08-25 01:57:08 +0900

    [style] 알림 없을 때 텍스트 화면 중앙으로 위치 조정

--- feature/notification/src/main/kotlin/com/tripmate/android/notification/NotificationScreen.kt
+++ feature/notification/src/main/kotlin/com/tripmate/android/notification/NotificationScreen.kt
@@ -2,6 +2,7 @@ package com.tripmate.android.notification
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -11,7 +12,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -71,16 +71,13 @@ fun NotificationScreen(
                 navigationType = TopAppBarNavigationType.None,
                 title = stringResource(id = R.string.notification_title),
             )
-            LazyColumn {
-                item { Spacer(modifier = Modifier.height(5.dp)) }
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = if (notificationUiState.notificationList.isEmpty()) Arrangement.Center
+                else Arrangement.Top,
+            ) {
                 if (notificationUiState.notificationList.isEmpty()) {
-                    item {
-                        EmptyNotificationItem(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(248.dp),
-                        )
-                    }
+                    item { EmptyNotificationItem() }
                 } else {
                     notificationUiState.notificationList.forEach { (date, notifications) ->
                         item {
