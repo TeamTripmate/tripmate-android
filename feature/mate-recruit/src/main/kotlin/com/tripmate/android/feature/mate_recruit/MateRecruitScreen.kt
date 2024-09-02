b6a482dc5d7dd6a39ac9d53e3089aa7da68a7eff
easyhooon <mraz3068@gmail.com>
2024-09-03 00:40:59 +0900

    [chore] MateRecruit, MateReviewScreen 필요 없는 코드 제거

--- feature/mate-recruit/src/main/kotlin/com/tripmate/android/feature/mate_recruit/MateRecruitScreen.kt
+++ feature/mate-recruit/src/main/kotlin/com/tripmate/android/feature/mate_recruit/MateRecruitScreen.kt
@@ -62,7 +62,6 @@ fun MateRecruitRoute(
     viewModel: MateRecruitViewModel = hiltViewModel(),
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-    LocalContext.current
 
     ObserveAsEvents(flow = viewModel.uiEvent) { event ->
         when (event) {
@@ -128,217 +127,215 @@ fun MateRecruitContent(
     onAction: (MateRecruitUiAction) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Box {
-        Column(
-            modifier = modifier
-                .fillMaxSize()
-                .verticalScroll(rememberScrollState())
-                .padding(horizontal = 16.dp),
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(horizontal = 16.dp),
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = stringResource(id = R.string.mate_recruit_description),
+            style = Large20_Bold,
+            color = Color.Black,
+        )
+        Spacer(modifier = Modifier.height(28.dp))
+        Text(
+            text = stringResource(id = R.string.trip_location),
+            style = Medium16_SemiBold,
+            color = Gray001,
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp)
+                .clip(RoundedCornerShape(8.dp))
+                .background(Gray010),
         ) {
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(
-                text = stringResource(id = R.string.mate_recruit_description),
-                style = Large20_Bold,
-                color = Color.Black,
-            )
-            Spacer(modifier = Modifier.height(28.dp))
-            Text(
-                text = stringResource(id = R.string.trip_location),
-                style = Medium16_SemiBold,
-                color = Gray001,
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(60.dp)
-                    .clip(RoundedCornerShape(8.dp))
-                    .background(Gray010),
-            ) {
-                Column(
-                    modifier = Modifier
-                        .padding(horizontal = 16.dp)
-                        .align(Alignment.CenterStart),
-                ) {
-                    Text(
-                        text = uiState.tripLocation,
-                        style = Small14_SemiBold,
-                        color = Gray001,
-                    )
-                    Spacer(modifier = Modifier.height(4.dp))
-                    Text(
-                        text = uiState.tripLocationAddress,
-                        style = XSmall12_Reg,
-                        color = Gray004,
-                    )
-                }
-            }
-            Spacer(modifier = Modifier.height(28.dp))
-            Text(
-                text = stringResource(id = R.string.mate_recruit_title),
-                style = Medium16_SemiBold,
-                color = Gray001,
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            TripmateTextField(
-                text = uiState.mateRecruitTitle,
-                onTextChange = { text -> onAction(MateRecruitUiAction.OnMateRecruitTitleUpdated(text)) },
-                searchTextHintRes = R.string.mate_recruit_title_hint,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(52.dp),
-                maxLength = 25,
-            )
-            Spacer(modifier = Modifier.height(28.dp))
-            Text(
-                text = stringResource(id = R.string.schedule),
-                style = Medium16_SemiBold,
-                color = Gray001,
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Row {
-                Box(
-                    modifier = Modifier
-                        .weight(1f)
-                        .border(1.dp, Gray008, RoundedCornerShape(8.dp))
-                        .clip(RoundedCornerShape(8.dp))
-                        .padding(vertical = 16.dp, horizontal = 12.dp)
-                        .clickable { onAction(MateRecruitUiAction.OnScheduleDateClicked) },
-                ) {
-                    Text(
-                        text = uiState.mateRecruitDate,
-                        style = Small14_SemiBold,
-                        color = if (uiState.isMateRecruitDateUpdated) Gray001 else Gray007,
-                    )
-                }
-                Spacer(modifier = Modifier.width(8.dp))
-                Box(
-                    modifier = Modifier
-                        .weight(1f)
-                        .border(1.dp, Gray008, RoundedCornerShape(8.dp))
-                        .clip(RoundedCornerShape(8.dp))
-                        .padding(vertical = 16.dp, horizontal = 12.dp)
-                        .clickable { onAction(MateRecruitUiAction.OnScheduleTimeClicked) },
-                ) {
-                    Text(
-                        text = uiState.mateRecruitTime,
-                        style = Small14_SemiBold,
-                        color = if (uiState.isMateRecruitTimeUpdated) Gray001 else Gray007,
-                    )
-                }
-            }
-            Spacer(modifier = Modifier.height(28.dp))
-            Text(
-                text = stringResource(id = R.string.mate_recruit_content),
-                style = Medium16_SemiBold,
-                color = Gray001,
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            TripmateTextField(
-                text = uiState.mateRecruitContent,
-                onTextChange = { text -> onAction(MateRecruitUiAction.OnMateRecruitContentUpdated(text)) },
-                searchTextHintRes = R.string.mate_recruit_content_hint,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(200.dp),
-                multiline = true,
-                maxLength = 200,
-            )
-            Spacer(modifier = Modifier.height(28.dp))
-            Text(
-                text = stringResource(id = R.string.mate_type),
-                style = Medium16_SemiBold,
-                color = Gray001,
-            )
-            Spacer(modifier = Modifier.height(12.dp))
             Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center,
-            ) {
-                MateRecruitCheckBox(
-                    text = stringResource(R.string.similar_mate_type),
-                    isSelected = uiState.selectedMateType == MateType.SIMILAR,
-                    onSelectedChange = { onAction(MateRecruitUiAction.OnMateTypeSelected(MateType.SIMILAR)) },
-                    iconRes = R.drawable.ic_mate_type,
-                    checkedIconRes = R.drawable.ic_mate_type_checked,
-                )
-                Spacer(modifier = Modifier.height(10.dp))
-                MateRecruitCheckBox(
-                    text = stringResource(R.string.all_mate_type),
-                    isSelected = uiState.selectedMateType == MateType.ALL,
-                    onSelectedChange = { onAction(MateRecruitUiAction.OnMateTypeSelected(MateType.ALL)) },
-                    iconRes = R.drawable.ic_mate_type,
-                    checkedIconRes = R.drawable.ic_mate_type_checked,
-                )
-            }
-            Spacer(modifier = Modifier.height(40.dp))
-            Text(
-                text = stringResource(id = R.string.setting_gender_age),
-                style = Medium16_SemiBold,
-                color = Gray001,
-            )
-            Spacer(modifier = Modifier.height(12.dp))
-            LazyColumn(
-                modifier = modifier.height((uiState.allGenderAgeGroups.size * 32).dp),
-                verticalArrangement = Arrangement.spacedBy(10.dp),
-            ) {
-                items(
-                    count = uiState.allGenderAgeGroups.size,
-                    key = { index -> uiState.allGenderAgeGroups[index].id },
-                ) { index ->
-                    MateRecruitCheckBox(
-                        text = stringResource(id = uiState.allGenderAgeGroups[index].textResId),
-                        isSelected = uiState.selectedGenderAgeGroups.contains(uiState.allGenderAgeGroups[index]),
-                        onSelectedChange = {
-                            if (uiState.selectedGenderAgeGroups.contains(uiState.allGenderAgeGroups[index])) {
-                                onAction(MateRecruitUiAction.OnGenderAgeGroupDeselected(uiState.allGenderAgeGroups[index]))
-                            } else {
-                                onAction(MateRecruitUiAction.OnGenderAgeGroupSelected(uiState.allGenderAgeGroups[index]))
-                            }
-                        },
-                        iconRes = R.drawable.ic_mate_setting,
-                        checkedIconRes = R.drawable.ic_mate_setting_checked,
-                    )
-                }
-            }
-            Spacer(modifier = Modifier.height(40.dp))
-            Text(
-                text = stringResource(id = R.string.open_kakao_link),
-                style = Medium16_SemiBold,
-                color = Gray001,
-            )
-            Spacer(modifier = Modifier.height(4.dp))
-            Text(
-                text = stringResource(id = R.string.open_kakao_link_description),
-                style = XSmall12_Reg,
-                color = Gray004,
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            TripmateTextField(
-                text = uiState.openKakaoLink,
-                onTextChange = { text -> onAction(MateRecruitUiAction.OnOpenKakaoLinkUpdated(text)) },
-                searchTextHintRes = R.string.open_kakao_link_hint,
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(52.dp),
-            )
-            Spacer(modifier = Modifier.height(40.dp))
-            TripmateButton(
-                onClick = { onAction(MateRecruitUiAction.OnDoneClicked) },
-                modifier = Modifier.fillMaxWidth(),
-                contentPadding = PaddingValues(vertical = 18.dp),
-                enabled = uiState.mateRecruitTitle.isNotEmpty() &&
-                    uiState.mateRecruitContent.isNotEmpty() &&
-                    uiState.selectedGenderAgeGroups.isNotEmpty() &&
-                    uiState.openKakaoLink.isNotEmpty(),
+                    .padding(horizontal = 16.dp)
+                    .align(Alignment.CenterStart),
             ) {
                 Text(
-                    text = stringResource(R.string.done),
-                    style = Medium16_SemiBold,
+                    text = uiState.tripLocation,
+                    style = Small14_SemiBold,
+                    color = Gray001,
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(
+                    text = uiState.tripLocationAddress,
+                    style = XSmall12_Reg,
+                    color = Gray004,
                 )
             }
-            Spacer(modifier = Modifier.height(55.dp))
         }
+        Spacer(modifier = Modifier.height(28.dp))
+        Text(
+            text = stringResource(id = R.string.mate_recruit_title),
+            style = Medium16_SemiBold,
+            color = Gray001,
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        TripmateTextField(
+            text = uiState.mateRecruitTitle,
+            onTextChange = { text -> onAction(MateRecruitUiAction.OnMateRecruitTitleUpdated(text)) },
+            searchTextHintRes = R.string.mate_recruit_title_hint,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(52.dp),
+            maxLength = 25,
+        )
+        Spacer(modifier = Modifier.height(28.dp))
+        Text(
+            text = stringResource(id = R.string.schedule),
+            style = Medium16_SemiBold,
+            color = Gray001,
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Row {
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .border(1.dp, Gray008, RoundedCornerShape(8.dp))
+                    .clip(RoundedCornerShape(8.dp))
+                    .padding(vertical = 16.dp, horizontal = 12.dp)
+                    .clickable { onAction(MateRecruitUiAction.OnScheduleDateClicked) },
+            ) {
+                Text(
+                    text = uiState.mateRecruitDate,
+                    style = Small14_SemiBold,
+                    color = if (uiState.isMateRecruitDateUpdated) Gray001 else Gray007,
+                )
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .border(1.dp, Gray008, RoundedCornerShape(8.dp))
+                    .clip(RoundedCornerShape(8.dp))
+                    .padding(vertical = 16.dp, horizontal = 12.dp)
+                    .clickable { onAction(MateRecruitUiAction.OnScheduleTimeClicked) },
+            ) {
+                Text(
+                    text = uiState.mateRecruitTime,
+                    style = Small14_SemiBold,
+                    color = if (uiState.isMateRecruitTimeUpdated) Gray001 else Gray007,
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(28.dp))
+        Text(
+            text = stringResource(id = R.string.mate_recruit_content),
+            style = Medium16_SemiBold,
+            color = Gray001,
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        TripmateTextField(
+            text = uiState.mateRecruitContent,
+            onTextChange = { text -> onAction(MateRecruitUiAction.OnMateRecruitContentUpdated(text)) },
+            searchTextHintRes = R.string.mate_recruit_content_hint,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp),
+            multiline = true,
+            maxLength = 200,
+        )
+        Spacer(modifier = Modifier.height(28.dp))
+        Text(
+            text = stringResource(id = R.string.mate_type),
+            style = Medium16_SemiBold,
+            color = Gray001,
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            MateRecruitCheckBox(
+                text = stringResource(R.string.similar_mate_type),
+                isSelected = uiState.selectedMateType == MateType.SIMILAR,
+                onSelectedChange = { onAction(MateRecruitUiAction.OnMateTypeSelected(MateType.SIMILAR)) },
+                iconRes = R.drawable.ic_mate_type,
+                checkedIconRes = R.drawable.ic_mate_type_checked,
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            MateRecruitCheckBox(
+                text = stringResource(R.string.all_mate_type),
+                isSelected = uiState.selectedMateType == MateType.ALL,
+                onSelectedChange = { onAction(MateRecruitUiAction.OnMateTypeSelected(MateType.ALL)) },
+                iconRes = R.drawable.ic_mate_type,
+                checkedIconRes = R.drawable.ic_mate_type_checked,
+            )
+        }
+        Spacer(modifier = Modifier.height(40.dp))
+        Text(
+            text = stringResource(id = R.string.setting_gender_age),
+            style = Medium16_SemiBold,
+            color = Gray001,
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        LazyColumn(
+            modifier = modifier.height((uiState.allGenderAgeGroups.size * 32).dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+        ) {
+            items(
+                count = uiState.allGenderAgeGroups.size,
+                key = { index -> uiState.allGenderAgeGroups[index].id },
+            ) { index ->
+                MateRecruitCheckBox(
+                    text = stringResource(id = uiState.allGenderAgeGroups[index].textResId),
+                    isSelected = uiState.selectedGenderAgeGroups.contains(uiState.allGenderAgeGroups[index]),
+                    onSelectedChange = {
+                        if (uiState.selectedGenderAgeGroups.contains(uiState.allGenderAgeGroups[index])) {
+                            onAction(MateRecruitUiAction.OnGenderAgeGroupDeselected(uiState.allGenderAgeGroups[index]))
+                        } else {
+                            onAction(MateRecruitUiAction.OnGenderAgeGroupSelected(uiState.allGenderAgeGroups[index]))
+                        }
+                    },
+                    iconRes = R.drawable.ic_mate_setting,
+                    checkedIconRes = R.drawable.ic_mate_setting_checked,
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(40.dp))
+        Text(
+            text = stringResource(id = R.string.open_kakao_link),
+            style = Medium16_SemiBold,
+            color = Gray001,
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            text = stringResource(id = R.string.open_kakao_link_description),
+            style = XSmall12_Reg,
+            color = Gray004,
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        TripmateTextField(
+            text = uiState.openKakaoLink,
+            onTextChange = { text -> onAction(MateRecruitUiAction.OnOpenKakaoLinkUpdated(text)) },
+            searchTextHintRes = R.string.open_kakao_link_hint,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(52.dp),
+        )
+        Spacer(modifier = Modifier.height(40.dp))
+        TripmateButton(
+            onClick = { onAction(MateRecruitUiAction.OnDoneClicked) },
+            modifier = Modifier.fillMaxWidth(),
+            contentPadding = PaddingValues(vertical = 18.dp),
+            enabled = uiState.mateRecruitTitle.isNotEmpty() &&
+                uiState.mateRecruitContent.isNotEmpty() &&
+                uiState.selectedGenderAgeGroups.isNotEmpty() &&
+                uiState.openKakaoLink.isNotEmpty(),
+        ) {
+            Text(
+                text = stringResource(R.string.done),
+                style = Medium16_SemiBold,
+            )
+        }
+        Spacer(modifier = Modifier.height(55.dp))
     }
 }
 
