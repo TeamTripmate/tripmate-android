49437cad023e86ca0fda22f7a88d30442851af4d
jihun.lee <jihun.lee@medisolution.co.kr>
2024-09-09 09:38:29 +0900

    [chore] style check success

--- feature/home/src/main/kotlin/com/tripmate/android/feature/home/HomeScreen.kt
+++ feature/home/src/main/kotlin/com/tripmate/android/feature/home/HomeScreen.kt
@@ -1,7 +1,6 @@
 package com.tripmate.android.feature.home
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
