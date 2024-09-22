77f21d331a73d42fac9ea9a376a48c972bb3d493
easyhooon <mraz3068@gmail.com>
2024-09-23 00:11:13 +0900

    [feat] 로그인 API 연동을 위한 Retrofit 분기 처리

--- core/network/src/main/kotlin/com/tripmate/android/core/network/RetrofitQualifier.kt
+++ core/network/src/main/kotlin/com/tripmate/android/core/network/RetrofitQualifier.kt
@@ -4,8 +4,16 @@ import javax.inject.Qualifier
 
 @Qualifier
 @Retention(AnnotationRetention.BINARY)
+annotation class LoginClient
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
 annotation class TripmateClient
 
 @Qualifier
 @Retention(AnnotationRetention.BINARY)
 annotation class TripmateApi
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class LoginApi
